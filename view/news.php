5d24f4192f9f2615da048e563ce1bb5212c1f412
Fauvet <g.fauvet.perso@gmail.com>
2015-12-04 05:25:44 +0100

    News améliorées

--- view/news.php
+++ view/news.php
@@ -10,24 +10,24 @@
 
 <div class='post'>
 	<img src='src/miniature1.jpg'/>
-	<h6>Jean-Michel Dutrou</h6>
+	<h6>Un poney aperçu à la Nuit de l'Informatique au Mirail.</h6>
 	<p>
-		coucou<br> ça va?<br>sqldkjqslkdjq<br>sdfjdslfkjdslkfjsdkljflksdjfsdkjflksdjfmsljkdfmqlsjkfmqlsdjkfmqsdlkjfmldsqkjfmlqsdjflskj<br>sdkjflskdf
+		Plus tôt dans la soirée a été aperçu un poney. Nous vous recommendons donc la plus grande vigilance.	
 	</p>
 
 
 	<div class='postChild'>
 		<img src='src/miniature1.jpg'/>
-		<h6>Jean-Michel Dutrou</h6>
+		<h6>2 victimes suite à la rédaction de prévention.</h6>
 		<p>
-			coucou<br> ça va?<br>sqldkjqslkdjq<br>sdfjdslfkjdslkfjsdkljflksdjfsdkjflksdjfmsljkdfmqlsjkfmqlsdjkfmqsdlkjfmldsqkjfmlqsdjflskj<br>sdkjflskdf
+			Il s'agirait plus exactement du poney lui-même et du respect, tout deux aurait alors disparu quelques heures plus tôt.
 		</p>
 	</div>
 	<div class='postChild'>
 		<img src='src/miniature1.jpg'/>
-		<h6>Jean-Michel Dutrou</h6>
+		<h6>Finalement le poney en question était une licorne.</h6>
 		<p>
-			coucou<br> ça va?<br>sqldkjqslkdjq<br>sdfjdslfkjdslkfjsdkljflksdjfsdkjflksdjfmsljkdfmqlsjkfmqlsdjkfmqsdlkjfmldsqkjfmlqsdjflskj<br>sdkjflskdf
+			Alors que personne ne comprenait pourquoi le poney avait un énorme ongle sur le front, des scientifiques ont pu alors prouvé qu'il s'agissait bel et bien d'une licorne.
 		</p>
 	</div>
 </div>
@@ -38,17 +38,19 @@
 
 <div class='post'>
 	<img src='src/miniature1.jpg'/>
-	<h6>Jean-Michel Dutrou</h6>
+	<h6>Tsunamie en provenance de la mer méditerrannée devrait arrivé à Paris d'ici 12h.</h6>
 	<p>
-		coucou<br> ça va?<br>sqldkjqslkdjq<br>sdfjdslfkjdslkfjsdkljflksdjfsdkjflksdjfmsljkdfmqlsjkfmqlsdjkfmqsdlkjfmldsqkjfmlqsdjflskj<br>sdkjflskdf
+		Plus tôt dans la soirée un plongeur à sauté du haut d'un avion et à fait un plat. Heureusement pour lui, Aurélien Clérac alors 
+		nouveau recorman du plus haut plongeon ne s'en tire qu'avec une légère commotion cérébrale. Toutefois, le plat a ainsi généré un tsunamie 
+		de plus de 3km de hauteur au dessus du niveau de la mer.
 	</p>
 
 
 	<div class='postChild'>
 		<img src='src/miniature1.jpg'/>
-		<h6>Jean-Michel Dutrou</h6>
+		<h6>Perpignan ravagé.</h6>
 		<p>
-			coucou<br> ça va?<br>sqldkjqslkdjq<br>sdfjdslfkjdslkfjsdkljflksdjfsdkjflksdjfmsljkdfmqlsjkfmqlsdjkfmqsdlkjfmldsqkjfmlqsdjflskj<br>sdkjflskdf
+			Alors que le Tsunamie s'approche de la côte. La ville de Perpignan est la première ville atteinte par cette catastrophe.
 		</p>
 	</div>
 	<div class='postChild'>
